b6416535f751a8f97ec8885432792e85cf85f0dd
Juliya <yula_sakura@list.ru>
2023-02-17 22:25:40 +0300

    commit

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -3,20 +3,17 @@ import './checkBox.css'
 
 export type CheckBoxProps = Omit<React.InputHTMLAttributes<HTMLInputElement>,
     'onChange'> & {
-  /** Вызывается при клике на чекбокс */
-  onChange: (value: boolean) => void;
+    /** Вызывается при клике на чекбокс */
+    onChange: (value: boolean) => void;
 };
 
 export const CheckBox: React.FC<CheckBoxProps> = (props) => {
-    console.log(props)
     return (
         <input
             {...props}
-
             className="checkbox"
             type="checkbox"
             checked={props.checked}
-            // checked={props.checked ? true : false}
             onChange={(e) => props.onChange(!props.checked)}
         />
     )
